e9b6803da7b70c31c166a02b93663a19f55ea161
fita8547 <junsumon090608@gmail.com>
2026-06-24 14:55:01 +0900

    first commit

---
+++ src/main/java/com/dgsw/meetlog/MeetLogApplication.java
@@ -0,0 +1,13 @@
+package com.dgsw.meetlog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MeetLogApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MeetLogApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/dgsw/meetlog/MeetLogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dgsw.meetlog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MeetLogApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
