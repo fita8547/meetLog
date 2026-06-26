f3aea5d94a16781f8e876959893c03eeb7bbdef8
fita8547 <junsumon090608@gmail.com>
2026-06-26 10:34:08 +0900

    fix

--- src/main/java/com/dgsw/meetlog/material/repository/MaterialRepository.java
+++ src/main/java/com/dgsw/meetlog/material/repository/MaterialRepository.java
@@ -3,5 +3,5 @@ package com.dgsw.meetlog.material.repository;
 import com.dgsw.meetlog.material.entity.Material;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface MaterialRepository extends JpaRepository<Material, Long> {
+public interface MaterialRepository extends JpaRepository<Material, Integer> {
 }
